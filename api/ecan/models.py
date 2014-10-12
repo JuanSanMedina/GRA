d47cfea64fc274ba6edc0155dc2f295dbcdc2e72
Sean <sean@emrals.com>
2014-10-12 00:44:36 -0400

    commit

---
+++ api/ecan/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Ecan(models.Model):
+    address = models.CharField(max_length=255, blank=True, null=True)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    modified = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+class Item(models.Model):
+    ecan = models.ForeignKey(Ecan)
+    user = models.ForeignKey(User,blank=True, null=True)
+    image_color = models.ImageField(upload_to="items")
+    image_ir = models.ImageField(upload_to="recognition")
+    weight = models.CharField(max_length=255, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    modified = models.DateTimeField(auto_now=True, blank=True, null=True)
