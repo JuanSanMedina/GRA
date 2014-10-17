71c0c14c6ea56c94bb6d46f97981deb11a501c9a
Juan Santiago Medina <juansantiago.medina@gmail.com>
2014-10-17 00:48:28 -0400

    minor changes

--- api/settings.py
+++ api/settings.py
@@ -129,10 +129,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 MEDIA_URL = '/media/'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'app22474969@heroku.com'
