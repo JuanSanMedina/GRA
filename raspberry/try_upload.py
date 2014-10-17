c4251ccd4b9d88c9f95bbb10bdba4cfc23542136
Juan Santiago Medina <juansantiago.medina@gmail.com>
2014-10-17 14:16:29 -0400

    upload

--- raspberry/try_upload.py
+++ raspberry/try_upload.py
@@ -1,11 +1,11 @@
 import requests
 # from pprint import pprint
 
-url = 'http://127.0.0.1:8000/ecan/upload/'
+# url = 'http://127.0.0.1:8000/ecan/upload/'
+url = 'http://ecan-recognition.herokuapp.com/ecan/upload/'
 
 data = {'ecan':'1', 'weight':'2.3'}
-files = {'image_color': open('can.jpeg'),'image_ir':open('ir_can.jpg')}
+files = {'image_color': open('can.jpeg', 'rb'),'image_ir':open('ir_can.jpg', 'rb')}
 
 r = requests.post(url, data = data, files=files)
-
-response = requests.post('http://127.0.0.1:8000/ecan/upload/', files)
+print r.text
