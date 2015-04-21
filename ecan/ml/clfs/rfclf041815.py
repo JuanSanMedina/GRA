89143df8d4f2add1c19abb0a38f5f61828a5a1c0
JuanSanMedina <juansantiago.medina@gmail.com>
2015-04-21 00:36:58 -0400

    fit whole forest

--- ecan/ml/clfs/rfclf041815.py
+++ ecan/ml/clfs/rfclf041815.py
@@ -1,39 +1,24 @@
-# Add path where ecan application resides
-import sys
-sys.path.append('../..')
-
-# Path where media resides
-media_path = 'api/site_media/media/'
-
-# needed to use foreing keys in models
-import django
-django.setup()
-
-# Modules
 import os
 import json
 import pickle
-from datetime import datetime
+import time
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import preprocessing
-from ecan.models import Item, Feature, Common_Name, Material, Shape, Logo
-
-hogs = Feature.objects.filter(name='hog_032415')[:100]
-# pixs = Feature.objects.filter(name='thumb_1:8')
-start_global = datetime.now()
-
-results = {}
-results[hogs[0].name] = {}
-results[hogs[0].name]['description'] = hogs[0].description
-results[pixs[0].name] = {}
-results[pixs[0].name]['description'] = pixs[0].description
-results['start'] = str(start_global)
+from ecan.models import Item, Feature, Common_Name, \
+                        Material, Shape, Logo
+import django
+from datetime import datetime
+import sys
+sys.path.append('../..')  # Add path where ecan application resides
+django.setup()  # needed to use foreing keys in models
+media_path = 'api/site_media/media/'  # Path where media resides
 
 
 def run():
-
-
+    results = {}
+    results['total_time'] = time.time()
+    hogs = Feature.objects.filter(name='hog_032415')
     target_matrix = np.array([
         [e.item.common_name.value,
                 e.item.material.value,
@@ -42,37 +27,38 @@ def run():
 
     le = preprocessing.LabelEncoder()
     le.fit(target_matrix[:, 0])
-    target = le.transform(target_matrix[:, 0])
-
-    a = {'hello': 'world'}
-
-    with open('filename.pickle', 'wb') as handle:
-      pickle.dump(a, handle)
-
-
-    # with open('filename.pickle', 'rb') as handle:
-    #   b = pickle.load(handle)
+    target = le.transform(target_matrix[:, 0]).tolist(),
+    data = np.array([e.feature for e in hogs])
 
     clf = RandomForestClassifier(n_estimators=100)
-    iris = datasets.load_iris()
-    X, y = iris.data, iris.target
+    X, y = data, target
     clf.fit(X, y)
 
-    fname = 'rfclf'
+    fname = 'rfclf_on_cn_%s.pkl' % datetime.now().strftime("%m%d%y")
+    lname = 'le_on_cn_%s.pickle' % datetime.now().strftime("%m%d%y")
     dirp = os.path.join('ecan', 'ml', 'clfs', fname)
     try:
         os.mkdir(dirp)
     except OSError as e:
         print str(e)
-        pass
 
-    fp = os.path.join(dirp, fname + '.pkl')
+    clf_p = os.path.join(dirp, fname)
+    le_p = os.path.join(dirp, lname)
 
     from sklearn.externals import joblib
-    joblib.dump(clf, fp)
+    joblib.dump(clf, clf_p)
 
-    clf2 = joblib.load(fp)
-    clf2.predict(X[0])
+    with open(le_p, 'wb') as f:
+        pickle.dump(le, f)
 
-    print y
-    pass
+    results['total_time'] = time.time() - results['total_time']
+    with open(dirp + 'results.json', 'w+') as f:
+        json.dump(results, f)
+
+    # with open('filename.pickle', 'rb') as handle:
+    #   b = pickle.load(handle)
+    # a = {'hello': 'world'}
+    # clf2 = joblib.load(fp)
+    # clf2.predict(X[0])
+    # print y
+    # pass
